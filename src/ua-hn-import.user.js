4d9b1275ef1046c67dfd6d990a9b70fe7b1f849b
Edj <edj@hermes>
2026-05-12 15:37:49 +0300

    feat: add entry point support for Residence venues

--- src/ua-hn-import.user.js
+++ src/ua-hn-import.user.js
@@ -1361,6 +1361,12 @@ const OVERPASS_TIMEOUT = 60000; // 60 seconds
           isResidential: true
         });
 
+        // Set as entry point (for navigation)
+        wmeSDK.DataModel.Venues.updateVenueEntryPoint({
+          venueId: String(venueId),
+          isEntryPoint: true
+        });
+
         // Select the new venue to open edit panel
         wmeSDK.Editing.setSelection({
           selection: {
@@ -2046,6 +2052,7 @@ const OVERPASS_TIMEOUT = 60000; // 60 seconds
         'DataModel.Venues.updateVenue',
         'DataModel.Venues.updateAddress',
         'DataModel.Venues.updateVenueIsResidential',
+        'DataModel.Venues.updateVenueEntryPoint',
         'DataModel.Venues.getAddress',
         'DataModel.Streets.getStreet',
         'DataModel.Streets.getById',
