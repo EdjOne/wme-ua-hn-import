3c5ef0aedcf5694086ab1ddd3036dee6546d7374
Edj <edj@hermes>
2026-05-14 14:07:56 +0300

    bump: v1.7.6 → v1.7.7

--- src/ua-hn-import.user.js
+++ src/ua-hn-import.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Quick RPP Importer - Ukraine
 // @namespace    https://github.com/EdjOne/wme-ua-hn-import
-// @version      1.7.6
+// @version      1.7.7
 // @description  Швидкий імпорт RPP UA 🇺🇦
 // @author       Edj (адаптація на основі ThatByte / zigapovhe)
 // @downloadURL  https://raw.githubusercontent.com/EdjOne/wme-ua-hn-import/main/src/ua-hn-import.user.js
