95c81fcf08223e42d516a4a69f24410533ad9eed
Edj <edj@hermes>
2026-05-16 12:47:18 +0300

    v1.7.81: tooltip right of marker

--- src/ua-hn-import.user.js
+++ src/ua-hn-import.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME UA-RPP
 // @namespace    https://github.com/EdjOne/house-number
-// @version      1.7.80
+// @version      1.7.81
 // @description  Швидкий імпорт RPP UA 🇺🇦
 // @author       EdjOne, Sapozhnik, Hermes Agent AI
 // @downloadURL  https://github.com/EdjOne/wme-ua-hn-import/raw/refs/heads/main/src/ua-hn-import.user.js
@@ -629,8 +629,8 @@
         const street = found.street || '';
         const num = found.number || '';
         tooltipEl.innerHTML = `${city ? city + '<br>' : ''}<b>${street || '—'}</b>${num ? ', ' + num : ''}`;
-        tooltipEl.style.left = (fPx.x + 25) + 'px';
-        tooltipEl.style.top = (fPx.y - 25) + 'px';
+        tooltipEl.style.right = (window.innerWidth - fPx.x + 10) + 'px';
+        tooltipEl.style.top = fPx.y + 'px';
         tooltipEl.style.display = 'block';
       } else {
         tooltipEl.style.display = 'none';
