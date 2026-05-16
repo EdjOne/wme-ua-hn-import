b14eee0930c4bcd19fbb1ecf1816f482552f1b33
Edj <edj@hermes>
2026-05-16 20:28:54 +0300

    v1.8.20: debug ol-viewport exists

--- src/ua-hn-import.user.js
+++ src/ua-hn-import.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME UA-RPP
 // @namespace    https://github.com/EdjOne/house-number
-// @version      1.8.19
+// @version      1.8.20
 // @description  Швидкий імпорт RPP UA 🇺🇦
 // @author       EdjOne, Sapozhnik, Hermes Agent AI
 // @downloadURL  https://github.com/EdjOne/wme-ua-hn-import/raw/refs/heads/main/src/ua-hn-import.user.js
@@ -638,7 +638,7 @@
         const vpRect = mapContainer.getBoundingClientRect();
         const sx = vpRect.left + foundPx.x + 5;
         const sy = vpRect.top + foundPx.y - 35;
-        console.log('Screen pos:', sx, sy, 'vpRect:', vpRect.left, vpRect.top, 'container:', mapContainer.tagName, mapContainer.className);
+        console.log('Screen pos:', sx, sy, 'vpRect:', vpRect.left, vpRect.top, 'container:', mapContainer.tagName, mapContainer.className, 'ol-vp exists:', !!document.querySelector('.ol-viewport'));
         tooltipEl.style.transform = `translate(${sx}px, ${sy}px)`;
         tooltipEl.style.display = 'block';
       } else {
