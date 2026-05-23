500c2631df5fb46b09691a25a4c2de770ee61cbb
Edj <edj@hermes>
2026-05-23 10:55:00 +0300

    bump: v1.8.27

--- src/ua-hn-import.user.js
+++ src/ua-hn-import.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME UA-RPP
 // @namespace    https://github.com/EdjOne/house-number
-// @version      1.8.26
+// @version      1.8.27
 // @description  Швидкий імпорт RPP UA 🇺🇦
 // @author       EdjOne, Sapozhnik, Hermes Agent AI
 // @downloadURL  https://github.com/EdjOne/wme-ua-hn-import/raw/refs/heads/main/src/ua-hn-import.user.js
